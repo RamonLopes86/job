bef353acb076a975528976a115b12532de139041
Ramon Lopes <dev.ramondasilvalopes@gmail.com>
2024-11-11 15:42:53 -0300

    finalizei o topo

---
+++ src/components/topo/topo.jsx
@@ -0,0 +1,37 @@
+
+import estiloTopo from './topo.module.css';
+import Link from 'next/link';
+
+
+
+
+export default function Topo(){
+
+
+    return(
+
+        <section className={estiloTopo.boxTopo}>
+
+            <nav>
+
+                <ul>
+                    <Link href={'#'} className={estiloTopo.linkTopo}><li>Sobre</li></Link>
+                    <Link href={'#'} className={estiloTopo.linkTopo}> <li>Quem Somos</li></Link>
+                    <Link href={'#'} className={estiloTopo.linkTopo}> <li>Modelos</li></Link>
+                    <Link href={'#'} className={estiloTopo.linkTopo}><li>Negócio</li></Link>
+                    <Link href={'#'} className={estiloTopo.linkTopo}> <li>Vantagens</li></Link>
+                    <Link href={'#'} className={estiloTopo.linkTopo}><li>Dúvidas Frequentes</li></Link>
+                   
+                </ul>
+
+                   
+            </nav>
+                    
+                 
+
+        </section>
+
+
+    )
+
+}
