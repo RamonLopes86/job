c1577cc862c33fd642079f28a5c3ed55256b5571
Ramon Lopes <dev.ramondasilvalopes@gmail.com>
2024-11-13 20:19:03 -0300

    add imagem

--- src/components/sobremin/sobre.jsx
+++ src/components/sobremin/sobre.jsx
@@ -25,7 +25,7 @@ export default function Sobre() {
 
                 <div className={estiloSobre.define}>
 
-                    <Image src={emo}/>
+                    <Image className={estiloSobre.imgEmoji} alt='imagem de um programador e seu computador' src={emo}/>
                     <p>Olá, me hamo Ramon , sou Desenvolvedor Web e vou te ajudar a com estratégias visuais a aprimorar sua imagem e seu alcançe profisioinal , transformando ideias em realidade com atendimento personalizado e consultoria vitalícia</p>
 
                 </div>
