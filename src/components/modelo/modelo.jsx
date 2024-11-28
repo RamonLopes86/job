d430dd0f5d191ac49455adde08e6f1bf2725b212
Ramon Lopes <dev.ramondasilvalopes@gmail.com>
2024-11-27 23:51:06 -0300

    criacao do router

--- src/components/modelo/modelo.jsx
+++ src/components/modelo/modelo.jsx
@@ -1,16 +1,109 @@
 import estiloMOdelo from './modelo.module.css'
 import Link from 'next/link'
+import Image from 'next/image'
+import arrayProj from '@/dados/arrProj'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowRight, faEye } from '@fortawesome/free-solid-svg-icons'
+import barber from '../../../public/barberCompleta.png'
 
 
-export default function Modelo(){
 
 
-    return(
 
-        <section>
+export default function Modelo() {
 
-                <h1>teste</h1>
-                <Link href={'/'} >  <button>Voltar</button></Link>
+
+
+
+
+
+
+    return (
+
+        <section className={estiloMOdelo.boxModelos}>
+
+            <div className={estiloMOdelo.boxButton}>
+
+                <Link href={'/'}> <button className={estiloMOdelo.btnSair}>  <FontAwesomeIcon className={estiloMOdelo.iconSeta} icon={faArrowRight} />   </button> </Link>
+
+            </div>
+
+
+
+
+            <section className={estiloMOdelo.modelos}>
+
+
+
+                {
+                    arrayProj.map((imagens) => {
+
+
+                        return (
+
+                            <div className={estiloMOdelo.cardd}>
+
+
+
+
+
+                                <div style={{ backgroundImage: `url(/${imagens.img}) `}} className={estiloMOdelo.imageContainer}>
+
+
+
+                                </div>
+
+
+
+                            </div>
+
+
+
+
+                        )
+
+
+
+                    })
+
+
+
+                }
+
+
+
+
+
+
+                {/* {
+
+                            arrayProj.map((image , index)=>{
+
+                                    return(
+
+
+                                        <div className={estiloMOdelo.card} key={index}>
+
+                                            <Image alt='imagens dos projetos desenvolvidos' className={estiloMOdelo.image} src={image.img}/>
+
+                                    
+                                            <Link href={''}> <button className={estiloMOdelo.btnVer}>Ver projeto  <FontAwesomeIcon className={estiloMOdelo.iconOlho} icon={faEye}/>   </button> </Link>
+                                          
+
+                                        </div>
+
+                                    )
+
+                            })
+
+
+                        }
+                     */}
+
+
+            </section>
+
+
 
         </section>
 
---
+++ src/dados/arrProj.js
@@ -0,0 +1,64 @@
+
+
+const arrayProj = [
+
+    {
+
+        img:'pousada.png',
+        link:''
+
+    },
+
+    {
+
+        img:'pet.png',
+        link:''
+
+    },
+    {
+
+        img:'rest.png',
+        link:''
+
+    },
+    {
+
+        img:'sorv.png',
+        link:''
+
+    },
+    {
+
+        img:'viagem.png',
+        link:''
+
+    },
+    {
+
+        img:'clinica.png',
+        link:''
+
+    },
+    {
+
+        img:'barberCompleta.png',
+        link:''
+
+    },
+    {
+
+        img:'alfa.png'
+
+    }
+
+
+
+]
+
+
+export default arrayProj
+
+
+
+
+
